493ee419df8d108f23047ecdea46bd040e6e6e45
mashrur ayon <mashrur399@gmail.com>
2023-08-13 22:44:52 +0600

    Initiating python packaging for genderstat

---
+++ genderstat-python/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='genderstat',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # add your dependencies here, e.g. 'numpy>=1.18.0'
+    ],
+    author='S M Mashrur Arafin Ayon',
+    author_email='mashrur399@gmail.com',
+    description='Quantitative Analysis Tools for Gender Studies in Python',
+    license='MIT',
+    keywords='gender analysis statistics',
+    url='https://github.com/mashrur-ayon/genderstat/tree/genderstat-python'
+)
